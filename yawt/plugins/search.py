17c282f7c2391b1864988e347fefa7124dd63e54
Desmond Rivet <dcr@desmondrivet.net>
2012-11-03 13:19:50 -0400

    more cleanup of search plugin

--- yawt/plugins/search.py
+++ yawt/plugins/search.py
@@ -16,7 +16,7 @@ class SearchView(object):
   
     def dispatch_request(self, flavour, category, search_text, page, page_size, base_url):
         articles = filter(lambda a: a.is_in_category(category),
-                          self._fetch_articles_by_text(search_text))
+                          self._fetch_articles_by_text(category, search_text))
         if len(articles) < 1:
             return self._yawtview.render_missing_resource()
         else:
@@ -30,7 +30,7 @@ class SearchView(object):
     def _search_title(self, search_text):
         return 'Search results for: "%s"' % search_text
  
-    def _fetch_articles_by_text(self, searchtext):
+    def _fetch_articles_by_text(self, category, searchtext):
         """
         Fetch collection of articles by searchtext.
         """
@@ -46,139 +46,64 @@ class SearchView(object):
                 for sr in search_results:
                     article = self._store.fetch_article_by_fullname(sr['fullname'])
                     results.append(article)
-        return results
+        results = filter(lambda a: a.fullname.startswith(category), results)
+        return sorted(results, key = lambda info: info.ctime, reverse=True)
 
-
-class CleanIndexer(object):
-    def __init__(self, store, index_dir, index_name, doc_root):
+class TextIndexer(object):
+    def __init__(self, store, index_dir, index_name, doc_root=None):
         self._store = store
         self._index_dir = index_dir
         self._index_name = index_name
         self._doc_root = doc_root
 
     def pre_walk(self):
-        if not os.path.exists(self._index_dir):
-            os.mkdir(self._index_dir)
+        self._writer = self._get_writer(clean = True)
+        
+    def visit_article(self, fullname):
+        if self._doc_root and not fullname.startswith(self._doc_root):
+            return
+        self._update_document(fullname)
+
+    def update(self, statuses):
+        self.pre_walk()
+        for fullname in statuses.keys():
+            status = statuses[fullname]
+            if status not in ['A','M','R']:
+                continue
+            self.visit_article(fullname)
+        self.post_walk()
+        
+    def post_walk(self):
+        self._writer.commit()
+        
+    def _update_document(self, fullname):
+        article = self._store.fetch_article_by_fullname(fullname)
+        mtime = os.path.getmtime(self._store._name2file(fullname))
+        self._writer.update_document(fullname = unicode(article.fullname),
+                                     mtime = mtime,
+                                     title = unicode(article.title),
+                                     content = unicode(article.content))
             
+    def _create_schema(self):
         schema = Schema(fullname=ID(stored=True, unique=True),
                         mtime=STORED,
                         title=TEXT(stored=True),
                         content=TEXT)
-             
-        ix = create_in(self._index_dir, schema = schema,
-                       indexname = self._index_name)
+        return schema
     
-        self._writer = ix.writer()
-        
-    def visit_article(self, fullname):
-        if not fullname.startswith(self._doc_root):
-            return
-        
-        article = self._store.fetch_article_by_fullname(fullname)
-        mtime = os.path.getmtime(self._store._name2file(fullname))
-        self._writer.add_document(fullname = unicode(article.fullname),
-                                  mtime = mtime,
-                                  title = unicode(article.title),
-                                  content = unicode(article.content))
-        
-    def post_walk(self):
-        self._writer.commit()
-
-class SubsetIndexer(object):
-    def __init__(self, store, index_dir, index_name, doc_root):
-        self._store = store
-        self._index_dir = index_dir
-        self._index_name = index_name
-        self._doc_root = doc_root
- 
-    def update(self, statuses):
-        writer = self._get_writer()
-
-        for fullname in statuses.keys():
-            if not fullname.startswith(self._doc_root):
-                continue
-            
-            status = statuses[fullname]
-            if status not in ['A','M','R']:
-                continue
-            
-            article = self._store.fetch_article_by_fullname(fullname)
-            mtime = os.path.getmtime(self._store._name2file(fullname))
-            writer.update_document(fullname = unicode(article.fullname),
-                                   mtime = mtime,
-                                   title = unicode(article.title),
-                                   content = unicode(article.content))
-        writer.commit()
-
-    def _get_writer(self):
+    def _get_writer(self, clean):
         if not os.path.exists(self._index_dir):
             os.mkdir(self._index_dir)
 
-        if not exists_in(self._index_dir, self._index_name):
-            schema = Schema(fullname=ID(stored=True, unique=True),
-                            mtime=STORED,
-                            title=TEXT(stored=True),
-                            content=TEXT)
-
+        if clean or not exists_in(self._index_dir, self._index_name):
+            schema = self._create_schema()
             ix = create_in(self._index_dir, schema = schema,
                            indexname = self._index_name)
         else:
             ix = open_dir(self._index_dir, indexname = self._index_name)
             
         return ix.writer()
-        
 
-# NOT USED
-class IncrementalIndexer(object):
-    def __init__(self, store, index_root, index_name):
-        self._store = store
-        self._index_root = index_root
-        self._index_name = index_name
-
-    def pre_walk(self):
-        ix = open_dir(self._index_root, indexname = self._index_name)
-        searcher = ix.searcher()
-        self._writer = ix.writer()
-
-        # The set of all paths in the index
-        self._indexed_paths = set()
-        
-        # The set of all paths we need to re-index
-        self._to_index = set()
-        
-        # Loop over the stored fields in the index
-        for fields in searcher.all_stored_fields():
-             indexed_name = fields['fullname']
-             path = self._store._name2file(indexed_name)
-             self._indexed_paths.add(path)
-             
-             if not os.path.exists(path):
-                 # This file was deleted since it was indexed
-                 self._writer.delete_by_term('fullname', indexed_name)
-             else:
-                 # Check if this file was changed since it
-                 # was indexed
-                 indexed_time = fields['mtime']
-                 mtime = os.path.getmtime(path)
-                 if mtime > indexed_time:
-                     # The file has changed, delete it and add it to the list of
-                     # files to reindex
-                     self._writer.delete_by_term('fullname', indexed_name)
-                     self._to_index.add(path)
-        
-    def visit_article(self, fullname):
-        filename = self.store._name2file(fullname)
-        if filename in self._to_index or filename not in self._indexed_paths:
-            # This is either a file that's changed, or a new file
-            # that wasn't indexed before. So index it!
-            article = self._store.fetch_article_by_fullname(af)
-            self._writer.add_document(fullname = unicode(article.fullname),
-                                      mtime = mtime,
-                                      title = unicode(article.title),
-                                      content = unicode(article.content))
-
-    def post_walk(self):
-        self._writer.commit()
 
 class SearchPlugin(object):
     def __init__(self):
@@ -191,8 +116,7 @@ class SearchPlugin(object):
     def init(self, app, plugin_name):
         self.app = app
         self.name = plugin_name
-    
-        # Search URL
+
         @app.route('/search/', methods=['POST', 'GET'])
         def full_text_search():
             return self._full_text_search(request, None, '')
@@ -218,20 +142,16 @@ class SearchPlugin(object):
             return self._full_text_search(request, flav, category)
 
     def walker(self, store):
-        return CleanIndexer(store, self._get_index_dir(), self._get_index_name(), self._get_base())
+        return TextIndexer(store, self._get_index_dir(), self._get_index_name())
 
     def updater(self, store):
-        return SubsetIndexer(store, self._get_index_dir(), self._get_index_name(), self._get_base())
+        return TextIndexer(store, self._get_index_dir(), self._get_index_name())
 
     def _get_index_dir(self):
         return yawt.util.get_abs_path_app(self.app, self._plugin_config()['INDEX_DIR'])
 
     def _get_index_name(self):
         return self._plugin_config()['INDEX_NAME']
-
-    def _get_base(self):
-        base = self._plugin_config()['BASE'].strip()
-        return base.rstrip('/')
 
     def _plugin_config(self):
         return self.app.config[self.name]
