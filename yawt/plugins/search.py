744068debb462187c505743ca5da7eff5b86a2ed
Desmond Rivet <dcr@desmondrivet.net>
2012-11-03 13:21:50 -0400

    change search plugin to use pluggable views

--- yawt/plugins/search.py
+++ yawt/plugins/search.py
@@ -3,32 +3,13 @@ import yawt
 
 from yawt.view import YawtView, PagingInfo
 from yawt.plugins.indexer import ArticleIndexer, ArticleFetcher
-from flask import g, request
+from flask import g, request, current_app
+from flask.views import View
+
 from whoosh.fields import Schema, STORED, ID, KEYWORD, TEXT
 from whoosh.index import create_in, open_dir, exists_in
 from whoosh.qparser import QueryParser
 
-
-class SearchView(object):
-    def __init__(self, fetcher, yawtview):
-        self._yawtview = yawtview
-        self._fetcher = fetcher
-  
-    def dispatch_request(self, flavour, category, search_text, page, page_size, base_url):
-        articles = self._fetcher.fetch(category, 'content', unicode(search_text))
-        if len(articles) < 1:
-            return self._yawtview.render_missing_resource()
-        else:
-            page_info = PagingInfo(page, page_size, len(articles), base_url)
-            return self._render_collection(flavour, articles, search_text, page_info, category)
-        
-    def _render_collection(self, flavour, articles, search_text, page_info, category):
-        title = self._search_title(search_text)
-        return self._yawtview.render_collection(flavour, articles, title, page_info, category)
-                                                      
-    def _search_title(self, search_text):
-        return 'Search results for: "%s"' % search_text
- 
 
 class TextIndexer(ArticleIndexer):
     def __init__(self, store, index_dir, index_name, doc_root=None):
@@ -41,6 +22,46 @@ class TextIndexer(ArticleIndexer):
         return {'title': unicode(article.title), 'content': unicode(article.content)}
     
 
+class SearchView(View):
+    methods = ['GET', 'POST']
+    
+    def __init__(self, plugin_config):
+        self._plugin_config = plugin_config
+   
+    def dispatch_request(self, flavour=None, category=''):
+        yawtview = YawtView(g.plugins, yawt.util.get_content_types())
+        fetcher = ArticleFetcher(g.store, self._get_index_dir(), self._get_index_name())
+        
+        page = 1
+        try:
+            page = int(request.args.get('page', '1'))
+        except ValueError:
+            page = 1
+
+        page_size = int(g.config['YAWT_PAGE_SIZE'])
+        
+        search_text = request.args.get('searchtext', '')
+ 
+        articles = fetcher.fetch(category, 'content', unicode(search_text))
+        if len(articles) < 1:
+            return yawtview.render_missing_resource()
+        else:
+            page_info = PagingInfo(page, page_size, len(articles), request.base_url)
+            return self._render_collection(yawtview, flavour, articles, search_text, page_info, category)
+        
+    def _render_collection(self, yawtview, flavour, articles, search_text, page_info, category):
+        title = self._search_title(search_text)
+        return yawtview.render_collection(flavour, articles, title, page_info, category)
+                                                      
+    def _search_title(self, search_text):
+        return 'Search results for: "%s"' % search_text
+
+    def _get_index_dir(self):
+        return yawt.util.get_abs_path_app(current_app, self._plugin_config['INDEX_DIR'])
+
+    def _get_index_name(self):
+        return self._plugin_config['INDEX_NAME']
+    
 class SearchPlugin(object):
     def __init__(self):
         self.default_config = {
@@ -53,30 +74,16 @@ class SearchPlugin(object):
         self.app = app
         self.name = plugin_name
 
-        @app.route('/search/', methods=['POST', 'GET'])
-        def full_text_search():
-            return self._full_text_search(request, None, '')
+        app.add_url_rule('/search/', view_func = self._view_func('full_text_search'))
+        app.add_url_rule('/<path:category>/search/',
+                         view_func = self._view_func('full_text_search_cat'))
+        app.add_url_rule('/search/index', view_func = self._view_func('full_text_search_index'))
+        app.add_url_rule('/<path:category>/search/index',
+                         view_func = self._view_func('full_text_search_index_cat'))
+        app.add_url_rule('/search/index.flav', view_func = self._view_func('full_text_search_index_flav'))
+        app.add_url_rule('/<path:category>/search/index.flav',
+                         view_func = self._view_func('full_text_search_index_flav_cat'))
         
-        @app.route('/<path:category>/search/', methods=['POST', 'GET'])
-        def full_text_search(category):
-            return self._full_text_search(request, None, category)
-        
-        @app.route('/search/index', methods=['POST', 'GET'])
-        def full_text_search_index():
-            return self._full_text_search(request, None, '')
-        
-        @app.route('/<path:category>/search/index', methods=['POST', 'GET'])
-        def full_text_search_index(category):
-            return self._full_text_search(request, None, category)
-        
-        @app.route('/search/index.<flav>', methods=['POST', 'GET'])
-        def full_text_search_index():
-            return self._full_text_search(request, flav, '')
-        
-        @app.route('/<path:category>/search/index.<flav>', methods=['POST', 'GET'])
-        def full_text_search_index(category):
-            return self._full_text_search(request, flav, category)
-
     def walker(self, store):
         return TextIndexer(store, self._get_index_dir(), self._get_index_name())
 
@@ -92,22 +99,9 @@ class SearchPlugin(object):
     def _plugin_config(self):
         return self.app.config[self.name]
     
-    def _full_text_search(self, request, flav, category):
-        page = 1
-        try:
-            page = int(request.args.get('page', '1'))
-        except ValueError:
-            page = 1
-            
-        search_text = request.args.get('searchtext', '')
-        sv = self._create_search_view()
-        return sv.dispatch_request(flav, category, search_text,
-                                   page, g.config['YAWT_PAGE_SIZE'], request.base_url)
+    def _view_func(self, endpoint):
+        return SearchView.as_view(endpoint, plugin_config = self._plugin_config())
 
-    def _create_search_view(self):
-        return SearchView(ArticleFetcher(g.store, self._get_index_dir(), self._get_index_name()),
-                          YawtView(g.plugins, yawt.util.get_content_types()))
-
-
+        
 def create_plugin():
     return SearchPlugin()
